c3686a65dcb910722b1d8cd06390b6ce0ceb5042
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2019-07-18 03:00:09 -0500

    added & modified.

---
+++ single-comic.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Marvel Comics API Search - Single Comic</title>
+
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body onload="singleCharacter()">
+    <div class="jumbotron">
+        <div class="container">
+            <h1 class="header-main-title single-comic__main-title">Comic Title</h1>
+            <div class="card mb-3">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                <img src="5bc0a8f388c90.jpg" class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">Comic Title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt fugit, voluptates blanditiis provident at harum. Magni, unde est sunt eligendi, dolore quis fugit id laborum iste tempora voluptas minima dolores? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo necessitatibus, ab laboriosam, sint perspiciatis et commodi voluptatibus hic magnam molestias ratione aliquam iure cupiditate, rem debitis quod autem aperiam deserunt? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut sapiente vitae quae dolore reprehenderit iste aut veritatis magnam cupiditate. Vero earum hic delectus, dignissimos at ipsam tempora optio. Eos, laboriosam.</p>
+                    <p class="card-text"><small class="text-muted">Characters: </small></p>
+                    <p class="card-text"><small class="text-muted">Creators: </small></p>
+                </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>    
+    
+    <script src="main.js"></script>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
