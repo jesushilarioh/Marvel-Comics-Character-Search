f23dc075f268ba7d7dad64436bc3a8b1b02fad73
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2019-07-28 21:08:50 -0500

    deleted & modified.

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 function character() {
     var urlQueryParameters = new URLSearchParams(window.location.search),
-        queryParameterName = urlQueryParameters.get('name');
+        queryParameterName = urlQueryParameters.get('name'),
+        name = document.getElementById('name').value;
 
     if(queryParameterName !== null && queryParameterName !== '') {
         document.getElementById('name').value = queryParameterName;
         connection();
-
-    } else {
+    } else if(name !== null && name !== ''){
         document.getElementById('connectionForm')
             .addEventListener('submit', connection);
-    
+    } else {
+        document.getElementById('characterSection')
+                .innerHTML = '<h2 id="characterMainTitle">Enter search term above...</h2>';
     }
 }
 
